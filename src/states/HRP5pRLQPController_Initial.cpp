7b45c9429e1fce622c1619196db4dd1a7d8d1670
bastien_muraccioli <bastien.muraccioli.pro@gmail.com>
2026-08-04 17:54:03 +0900

    Fix merge

--- src/states/HRP5pRLQPController_Initial.cpp
+++ src/states/HRP5pRLQPController_Initial.cpp
@@ -10,12 +10,14 @@ void HRP5pRLQPController_Initial::start(mc_control::fsm::Controller & ctl_)
   ctl.activateQPControl(true);
   ctl.activateTorqueControl(false);
   ctl.activateContactConstraints(true);
+  ctl.postureTask->target(ctl.defaultPostureTarget);
   ctl.solver().addTask(ctl.postureTask);
 }
 
 bool HRP5pRLQPController_Initial::run(mc_control::fsm::Controller & ctl_)
 {
   auto & ctl = static_cast<HRP5pRLQPController &>(ctl_);
+  // ctl.updateFootContactsFromForceSensors();
   return false;
 }
 
